13328bb01085df37b1d69d4cc6479eec02f686eb
Godfried Meesters <godfriedmeesters@gmail.com>
2020-12-26 22:02:03 +0100

    /logs

--- logger.ts
+++ logger.ts
@@ -1,12 +1,13 @@
 import winston from 'winston';
 const ecsFormat = require('@elastic/ecs-winston-format');
 require('dotenv').config();
+var path = require('path');
 
 export const logger = winston.createLogger({
     level: process.env.LOG_LEVEL,
     format: ecsFormat(),
     transports: [
         new winston.transports.Console(),
-        new winston.transports.File({ filename: 'scraper.log' }),
+        new winston.transports.File({ filename:  path.join('.', 'logs' , 'scraper.log') }),
     ]
 })
