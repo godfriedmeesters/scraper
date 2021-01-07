cef15766d971290215c794fea2981bb99b6909b0
Godfried Meesters <godfriedmeesters@gmail.com>
2021-01-07 18:27:42 +0100

    logged

--- logger.ts
+++ logger.ts
@@ -2,7 +2,7 @@
  * @ Author: Godfried Meesters <godfriedmeesters@gmail.com>
  * @ Create Time: 2020-11-25 15:21:23
  * @ Modified by: Godfried Meesters <godfriedmeesters@gmail.com>
- * @ Modified time: 2020-12-29 13:07:09
+ * @ Modified time: 2021-01-07 18:27:27
  * @ Description:
  */
 
@@ -21,10 +21,10 @@ const logger = winston.createLogger({
     ]
 })
 
-if (process.env.IN_DEV)  {
-    logger.add(new winston.transports.Console({
-        format: winston.format.simple(),
-    }));
-}
+
+logger.add(new winston.transports.Console({
+    format: winston.format.simple(),
+}));
+
 
 export { logger };
