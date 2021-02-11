ae01ad72c57769c0fe1c80b143f8f96fe0d7c369
Godfried Meesters <godfriedmeesters@gmail.com>
2021-02-11 12:13:04 +0100

    expedia web fixed

--- expedia/ExpediaWebScraper.ts
+++ expedia/ExpediaWebScraper.ts
@@ -69,8 +69,7 @@ export class ExpediaWebScraper extends WebScraper implements IScraper {
         await this.clickElementByText(this.translator.translate('Suchen'));
         await this.page.waitFor(10000);
 
-        await this.page.waitForXPath("//input[@data-test-id='stopFilter_stops-0' or @data-test-id='stops-0' ]");
-        await this.clickElementByXpath("//input[@data-test-id='stopFilter_stops-0' or @data-test-id='stops-0' ]");
+        await this.clickElementByXpath("//label[@for='stops-0']");
 
         await this.page.waitFor(5000);
 
