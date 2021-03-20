32406912ceaae30ceeb051f1cfeb510f4c9c6ed8
Godfried Meesters <godfriedmeesters@gmail.com>
2021-03-20 13:52:54 +0100

    booking web and app best/cheapest

--- companies/booking/BookingAppScraper.ts
+++ companies/booking/BookingAppScraper.ts
@@ -2,7 +2,7 @@
  * @ Author: Godfried Meesters <godfriedmeesters@gmail.com>
  * @ Create Time: 2020-12-03 15:04:24
  * @ Modified by: Godfried Meesters <godfriedmeesters@gmail.com>
- * @ Modified time: 2021-03-12 08:57:22
+ * @ Modified time: 2021-03-20 13:52:32
  * @ Description:
  */
 
@@ -10,7 +10,7 @@
 
 import { AppScraper } from "../../AppScraper";
 import { IScraper } from "../../IScraper";
-import { HotelOffer} from "../../types";
+import { HotelOffer } from "../../types";
 var path = require('path');
 
 import { logger } from "../../logger";
@@ -92,6 +92,20 @@ export class BookingAppScraper extends AppScraper implements IScraper {
 
     await this.sleep(10000);
 
+
+    const offersSortedByBest = await this.extractOffers();
+
+    await this.clickElementByResource('com.booking:id/sresults_sort');
+
+    await this.clickElementByXpath('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.CheckedTextView[6]');
+
+    const offersSortedByCheapest = await this.extractOffers();
+
+    return { 'sortedByBest': offersSortedByBest, 'sortedByCheapest': offersSortedByCheapest };
+
+  }
+
+  async extractOffers() {
     /*******************************************/
     var rect = await this.appiumClient.getWindowRect();
 
@@ -103,7 +117,7 @@ export class BookingAppScraper extends AppScraper implements IScraper {
     var hotelOffersOnScreen = []; //flight offers currently on the screen
     var hotelOffers = [];
 
-    var initPrices =  await this.getElementsByResourceId('com.booking:id/price_view_price');
+    var initPrices = await this.getElementsByResourceId('com.booking:id/price_view_price');
 
     var lastPriceOffScreenText = await initPrices[0].getText();
 
@@ -126,13 +140,13 @@ export class BookingAppScraper extends AppScraper implements IScraper {
         const hotelnameY = parseInt(bounds.match(/\d+/g)[1]);
 
         if (prices.length == 0) {
-          hotelOffer.price =  lastPriceOffScreenText;
+          hotelOffer.price = lastPriceOffScreenText;
         }
         else if (prices.length == 1) {
           bounds = await prices[0].getAttribute("bounds");
           const priceY = parseInt(bounds.match(/\d+/g)[1]);
           if (hotelnameY < priceY) {
-            hotelOffer.price =  lastPriceOffScreenText;
+            hotelOffer.price = lastPriceOffScreenText;
           }
           else {
             hotelOffer.price = await prices[0].getText();
@@ -162,10 +176,10 @@ export class BookingAppScraper extends AppScraper implements IScraper {
         const screenShotFlightOffer = { ...hotelOffer };
         screenShotFlightOffer.screenshot = screenshot;
 
-        if (_.findWhere(hotelOffers,  screenShotFlightOffer) == null) {
+        if (_.findWhere(hotelOffers, screenShotFlightOffer) == null) {
 
 
-          hotelOffers.push( screenShotFlightOffer);
+          hotelOffers.push(screenShotFlightOffer);
 
 
           logger.info("adding new hotel offer");
@@ -195,112 +209,8 @@ export class BookingAppScraper extends AppScraper implements IScraper {
 
     }
 
-    return  hotelOffers;
+    return hotelOffers;
 
-
-    // var rect = await this.appiumClient.getWindowRect();
-    // var rectX = rect.width / 3;
-    // var rectY = rect.height / 1.1;
-
-    // var hotelOffersOnScreen = []; //flight offers currently on the screen
-    // var hotelOffers = [];
-
-    // var initPrices = await this.getElementsByResourceId('com.booking:id/price_view_price');
-
-    // var lastPriceOffScreenText = await initPrices[0].getText();
-
-    // var bounds = await initPrices[0].getAttribute("bounds");
-    // var lastPriceOffScreenY = parseInt(bounds.match(/\d+/g)[1]);
-
-
-    // while (true) {
-    //   //console.log("last price offscreen = " + lastPriceOffScreenText)
-    //   var oldHotelOffersOnScreen = hotelOffersOnScreen.slice();
-    //   hotelOffersOnScreen = [];
-    //   var prices = await this.getElementsByResourceId('com.booking:id/price_view_price');
-    //   var hotelNames = await this.getElementsByResourceId('com.booking:id/sr_property_card_header_property_name');
-    //   var screenshot = await this.takeScreenShot(this.constructor.name);
-
-    //   logger.info("found " + prices.length  + " prices");
-
-    //   for (var i = 0; i < prices.length && i < hotelNames.length; i++) {
-    //     var hotelOffer = new HotelOffer();
-
-    //     var bounds = await hotelNames[i].getAttribute("bounds");
-    //     const hotelNameY = parseInt(bounds.match(/\d+/g)[1]);
-
-    //     if (prices.length == 0) {
-    //       hotelOffer.price = lastPriceOffScreenText;
-    //     }
-    //     else if (prices.length == 1) {
-    //       bounds = await prices[0].getAttribute("bounds");
-    //       const priceY = parseInt(bounds.match(/\d+/g)[1]);
-    //       if (hotelNameY < priceY) {
-    //         hotelOffer.price = lastPriceOffScreenText;
-    //       }
-    //       else {
-    //         hotelOffer.price = await prices[0].getText();
-    //       }
-    //     }
-    //     else if (prices.length > 1) {
-    //       if (hotelNameY > lastPriceOffScreenY) {
-    //         hotelOffer.price = lastPriceOffScreenText;
-    //       }
-
-    //       for (var j = 0; j < prices.length; j++) {
-    //         bounds = await prices[j].getAttribute("bounds");
-    //         const priceY = parseInt(bounds.match(/\d+/g)[1]);
-
-    //         if (hotelNameY > priceY) {
-    //           hotelOffer.price = await prices[j].getText();
-    //         }
-    //       }
-    //     }
-
-
-    //     hotelOffer.searchLocation = inputData.location;
-    //     hotelOffer.checkinDate = inputData.checkinDate;
-
-    //     hotelOffersOnScreen.push(hotelOffer);
-    //     logger.info("found hotel offer " + JSON.stringify(hotelOffer));
-
-    //     const screenShotHotelOffer = { ...hotelOffer };
-    //     screenShotHotelOffer.screenshot = screenshot;
-
-    //     if (_.findWhere(hotelOffers, screenShotHotelOffer) == null) {
-
-    //       screenShotHotelOffer.price = screenShotHotelOffer.price.replace("€", "").trim();
-    //       hotelOffers.push(screenShotHotelOffer);
-
-    //       logger.info("adding new hotel offer");
-    //     }
-    //     else {
-    //       logger.info("skipping hotel offer");
-    //     }
-    //   }
-
-    //   if (prices.length > 0) {
-    //     var bounds = await prices[prices.length - 1].getAttribute("bounds");
-    //     const newLastPriceOffScreenY = parseInt(bounds.match(/\d+/g)[1]);
-    //     lastPriceOffScreenY = newLastPriceOffScreenY;
-    //     lastPriceOffScreenText = await prices[prices.length - 1].getText();
-    //   }
-
-    //   if (_.isEqual(oldHotelOffersOnScreen, hotelOffersOnScreen)) {
-    //     break;
-    //   }
-
-
-    //   await this.appiumClient.touchAction([
-    //     { action: 'press', x: rectX, y: rectY },
-    //     { action: 'wait', ms: 500 },
-    //     { action: 'moveTo', x: rectX, y: rectY * 0.4 },
-    //     'release',
-    //   ]);
-
-    // }
-
-    // return hotelOffers;
 
   }
 
