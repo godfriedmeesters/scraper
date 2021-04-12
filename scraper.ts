64cfc5c1100f7453c57f6f5f1a36e7f94d7ca10c
Godfried Meesters <godfriedmeesters@gmail.com>
2021-04-12 20:42:21 +0200

    update

--- scraper.ts
+++ scraper.ts
@@ -2,7 +2,7 @@
  * @ Author: Godfried Meesters <godfriedmeesters@gmail.com>
  * @ Create Time: 2020-11-17 15:18:28
  * @ Modified by: Godfried Meesters <godfriedmeesters@gmail.com>
- * @ Modified time: 2021-04-12 19:57:39
+ * @ Modified time: 2021-04-12 20:42:12
  * @ Description:
  */
 
@@ -47,8 +47,8 @@ if (yn(process.env.PULL_WEB_BROWSER_QUEUE)) {
 
         const proxies = inputData.proxies;
 
-        //        var use_proxy = Math.random() < 0.8;
-        var use_proxy = false;
+               var use_proxy = Math.random() < 0.8;
+        //var use_proxy = false;
 
 
 
