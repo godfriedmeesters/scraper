aa09cc6a36c4fb19ce81f3dedfaac44261f03922
Godfried Meesters <godfriedmeesters@gmail.com>
2021-04-14 21:59:49 +0200

    fix

--- WebScraper.ts
+++ WebScraper.ts
@@ -2,7 +2,7 @@
  * @ Author: Godfried Meesters <godfriedmeesters@gmail.com>
  * @ Create Time: 2020-11-22 22:33:05
  * @ Modified by: Godfried Meesters <godfriedmeesters@gmail.com>
- * @ Modified time: 2021-04-14 21:30:29
+ * @ Modified time: 2021-04-14 21:49:29
  * @ Description:
  */
 
