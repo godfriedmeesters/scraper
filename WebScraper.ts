13774b59cbd1ce3e4b240cb8f2c043b76f34a6d0
Godfried Meesters <godfriedmeesters@gmail.com>
2021-04-15 22:05:56 +0200

    update in xpath timeout

--- WebScraper.ts
+++ WebScraper.ts
@@ -2,7 +2,7 @@
  * @ Author: Godfried Meesters <godfriedmeesters@gmail.com>
  * @ Create Time: 2020-11-22 22:33:05
  * @ Modified by: Godfried Meesters <godfriedmeesters@gmail.com>
- * @ Modified time: 2021-04-15 17:53:18
+ * @ Modified time: 2021-04-15 22:05:46
  * @ Description:
  */
 
@@ -278,7 +278,7 @@ class WebScraper {
         logger.info("Checking if xpath in page: " + xpath)
         await this.page.waitFor(1000);
         try {
-            if ((await this.page.waitForXPath(xpath, { timeout: 1500 })) !== null) {
+            if ((await this.page.waitForXPath(xpath, { timeout: 5000 })) !== null) {
                 return true;
             }
         }
