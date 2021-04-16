39db55814a694ab256209f00059a794791c8972d
Godfried Meesters <godfriedmeesters@gmail.com>
2021-04-16 09:56:49 +0200

    kayak web only from search

--- companies/kayak/KayakWebScraper.ts
+++ companies/kayak/KayakWebScraper.ts
@@ -2,7 +2,7 @@
  * @ Author: Godfried Meesters <godfriedmeesters@gmail.com>
  * @ Create Time: 2020-11-27 16:00:25
  * @ Modified by: Godfried Meesters <godfriedmeesters@gmail.com>
- * @ Modified time: 2021-03-27 23:54:32
+ * @ Modified time: 2021-04-16 09:53:40
  * @ Description:
  */
 
@@ -22,13 +22,77 @@ export class KayakWebScraper extends WebScraper implements IScraper {
 
     // scrape web part 1: until "clicking" the search button
     async scrapeUntilSearch(inputData: any) {
+        // const departureDate = inputData.departureDate;
+        // const origin = inputData.origin;
+        // const destination = inputData.destination;
+
+        // await this.page.goto(this.translator.translate("url"));
+
+
+        // await this.clickOptionalElementByCss(`//*[contains(@title, '${this.translator.translate("Akzeptieren")}')]`);
+        // await this.clickOptionalElementByText(this.translator.translate("Akzeptieren"));
+
+
+        // await this.clickOptionalElementByCss('#onetrust-accept-btn-handler');
+
+        // await this.clickOptionalElementByCss('.awaitBsvt-accept');
+        // await this.page.waitFor(5000);
+
+
+
+        // //await this.clickElementByXpath("//div[contains(@id, 'switch')]");
+
+        // await this.clickElementByCss(".Common-Widgets-Select-StyleJamSelect");
+
+        // await this.clickElementByXpath("//*[contains(@data-value, 'oneway')]");
+
+        // await this.clickElementByXpath("//button[contains(@class, 'remove-selection')]");
+
+        // await this.page.waitFor(500);
+
+        // const from1 = await this.page.$x("//input[contains(@id, 'origin-airport')]");
+        // await from1[0].type(origin, { delay: 50 });
+        // await this.tapEnter();
+
+        // await this.page.waitFor(500);
+        // await this.clickElementByXpath("//div[contains(@id, 'destination-airport')]");
+        // await this.page.waitFor(500);
+
+        // const to1 = await this.page.$x("//input[contains(@id, 'destination-airport')]");
+        // await to1[0].type(destination, { delay: 50 });
+        // await this.tapEnter();
+
+        // await this.page.waitFor(500);
+
+        // await this.clickElementByXpath("//div[contains(@id, 'dateRangeInput-display')]");
+
+        // this.page.waitFor(500);
+        // const selDate = await this.getElementByXpath(`//div[contains(@aria-label, '${this.translator.translate("Eingabe Abflugdatum")}')]`);
+
+        // this.page.waitFor(5000);
+        // if (this.language == "de")
+        //     await selDate.type(dateFormat(new Date(departureDate), 'dd.mm.yyyy'), { delay: 50 });
+        // else
+        //     await selDate.type(dateFormat(new Date(departureDate), 'dd/mm/yyyy'), { delay: 50 });
+
+
+        // await this.tapEnter();
+
+        // this.page.waitFor(500);
+
+    }
+
+    // scrape web part 2: from "clicking" on search button
+    async scrapeFromSearch(inputData) {
+
         const departureDate = inputData.departureDate;
         const origin = inputData.origin;
         const destination = inputData.destination;
 
-        await this.page.goto(this.translator.translate("url"));
+        const url = `${this.translator.translate("url")}/${origin}-${destination}/${departureDate}?fs=stops=0&sort=bestflight_a`;
+        await this.page.goto(url);
 
-
+        await this.page.waitFor(10000);
         await this.clickOptionalElementByCss(`//*[contains(@title, '${this.translator.translate("Akzeptieren")}')]`);
         await this.clickOptionalElementByText(this.translator.translate("Akzeptieren"));
 
@@ -36,58 +100,12 @@ export class KayakWebScraper extends WebScraper implements IScraper {
         await this.clickOptionalElementByCss('#onetrust-accept-btn-handler');
 
         await this.clickOptionalElementByCss('.awaitBsvt-accept');
-        await this.page.waitFor(5000);
 
 
+//WITH BUTTON
+       // await this.clickElementByXpath(`//button[contains(@title, '${this.translator.translate("Flüge suchen")}')]`);
 
-        //await this.clickElementByXpath("//div[contains(@id, 'switch')]");
-
-        await this.clickElementByCss(".Common-Widgets-Select-StyleJamSelect");
-
-        await this.clickElementByXpath("//*[contains(@data-value, 'oneway')]");
-
-        await this.clickElementByXpath("//button[contains(@class, 'remove-selection')]");
-
-        await this.page.waitFor(500);
-
-        const from1 = await this.page.$x("//input[contains(@id, 'origin-airport')]");
-        await from1[0].type(origin, { delay: 50 });
-        await this.tapEnter();
-
-        await this.page.waitFor(500);
-        await this.clickElementByXpath("//div[contains(@id, 'destination-airport')]");
-        await this.page.waitFor(500);
-
-        const to1 = await this.page.$x("//input[contains(@id, 'destination-airport')]");
-        await to1[0].type(destination, { delay: 50 });
-        await this.tapEnter();
-
-        await this.page.waitFor(500);
-
-        await this.clickElementByXpath("//div[contains(@id, 'dateRangeInput-display')]");
-
-        this.page.waitFor(500);
-        const selDate = await this.getElementByXpath(`//div[contains(@aria-label, '${this.translator.translate("Eingabe Abflugdatum")}')]`);
-
-        this.page.waitFor(5000);
-        if (this.language == "de")
-            await selDate.type(dateFormat(new Date(departureDate), 'dd.mm.yyyy'), { delay: 50 });
-        else
-            await selDate.type(dateFormat(new Date(departureDate), 'dd/mm/yyyy'), { delay: 50 });
-
-
-        await this.tapEnter();
-
-        this.page.waitFor(500);
-
-    }
-
-    // scrape web part 2: from "clicking" on search button
-    async scrapeFromSearch(inputData) {
-
-
-
-        await this.clickElementByXpath(`//button[contains(@title, '${this.translator.translate("Flüge suchen")}')]`);
+        /////////
 
         await this.page.waitFor(15000);
 
